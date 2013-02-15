7af13391885948842138ac3998b404a5211efb67
Nino Paolo <o70vd@yahoo.com>
2013-02-15 23:28:28 +0800

    Fixed minor issue.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,26 @@
-class laravel_framework {
+class laravel {
   exec { 'download-laravel':
     cwd     => '/tmp',
-    command => '/usr/bin/wget http://www.github.com/laravel/laravel/archive/develop.zip',
-    creates => '/tmp/download'
+    command => '/usr/bin/wget http://github.com/laravel/laravel/archive/develop.zip',
+    creates => '/tmp/develop.zip'
   }
   
   exec { 'unzip-laravel':
     cwd     => '/tmp',
-    command => "/usr/bin/unzip laravel-develop && /bin/mv laravel-develop/* ${docroot}/public",
-    require => [ Exec['download-laravel'] ]
+    command => "/usr/bin/unzip develop.zip && /bin/mv laravel-develop/* ${docroot}",
+    require => [ Package['unzip'], Exec['download-laravel'] ]
   }
 
-  file { ${docroot}:
+  file { "${docroot}":
     ensure  => 'directory'
   }
 
   $writeable_dirs = ["${docroot}/app/storage"]
-  file { ${writeable_dirs}:
+  file { "${writeable_dirs}":
     ensure  => 'directory',
     recurse => true,
     mode    => '0777',
-    require => File[${docroot}]
+    require => File["${docroot}"]
   }
 
   # add .htaccess
