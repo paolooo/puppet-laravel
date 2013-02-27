5e6580c2a52682b939a3ff0431ace557b994a2de
Nino Paolo <o70vd@yahoo.com>
2013-02-27 20:24:23 +0800

    Fixed the issue on moving `$ mv` laravel files to docroot. Added an option to execute `$ composer install`

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,33 +1,44 @@
-class laravel {
+class laravel (
+  $root = $docroot
+) {
+
+  file { "${root}":
+    ensure  => 'directory'
+  }
+
   exec { 'download-laravel':
     cwd     => '/tmp',
     command => '/usr/bin/wget http://github.com/laravel/laravel/archive/develop.zip',
     creates => '/tmp/develop.zip'
   }
+
   
   exec { 'unzip-laravel':
     cwd     => '/tmp',
-    command => "/usr/bin/unzip develop.zip && /bin/mv laravel-develop/* ${docroot}",
-    require => [ Package['unzip'], Exec['download-laravel'] ]
+    command => "/usr/bin/unzip develop.zip && /bin/cp -R laravel-develop/* ${root} && /bin/rm -rf develop.zip laravel-develop",
+    require => [ File["${root}"], Package['unzip'], Exec['download-laravel'] ]
   }
 
-  file { "${docroot}":
-    ensure  => 'directory'
-  }
 
-  $writeable_dirs = ["${docroot}/app/storage"]
+  $writeable_dirs = ["${root}/app/storage"]
   file { "${writeable_dirs}":
     ensure  => 'directory',
     recurse => true,
     mode    => '0777',
-    require => File["${docroot}"]
+    require => [ Exec["unzip-laravel"], File["${root}"] ]
+  }
+
+  exec { 'run-composer-installation':
+    cwd     => "${root}",
+    command => "composer install",
+    require => [ File["/usr/local/bin/composer"], Exec["unzip-laravel"], File["${root}"] ]
   }
 
   # add .htaccess
   file { 'add-htaccess':
-    path    => "${docroot}/public/.htaccess",
+    path    => "${root}/public/.htaccess",
     content => template('laravel/.htaccess'),
     ensure  => file,
-    require => [ Exec['unzip-laravel'] ]
+    require => [ Exec["run-composer-installation"], ]
   }
 }
